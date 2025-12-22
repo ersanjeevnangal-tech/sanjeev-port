dddfd649257e50be1d5fc3da068e3ceab6291482
Sanjeev Kumar <sanjeev.kumar@brihaspatitech.com>
2025-12-22 11:51:40 +0530

    added about

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./layout/Layout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
         {/* Layout wrapper */}
         <Route element={<Layout />}>
           <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+
         </Route>
 
         {/* Fallback */}
---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <div>
+        <div>About File</div>
+        <span>fhgfghf</span>
+        </div>
+    )
+}
